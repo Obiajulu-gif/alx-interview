e09de30ac4c74d4741156f795f13e8d3a272a608
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-15 21:24:11 +0100

    task 0 done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,16 @@ import sys
 import re
 
 total_file_size = 0
-status_code_count = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+status_code_count = {
+    str(code): 0 for code in [
+        200,
+        301,
+        400,
+        401,
+        403,
+        404,
+        405,
+        500]}
 pattern = re.compile(
     r'\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
