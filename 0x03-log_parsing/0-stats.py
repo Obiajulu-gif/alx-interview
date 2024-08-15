d79a794289d2ea257912e8999bf55b78e38ece7a
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-15 23:09:52 +0100

    task 0 done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,31 +16,34 @@ status_code_count = {
         404,
         405,
         500]}
-pattern = re.compile(
-    r'\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
+
 
 line_count = 0
 try:
     for line in sys.stdin:
-        line = line.strip()
-        match = pattern.search(line)
-        if match:
-            status_code, file_size = match.groups()
-            total_file_size += int(file_size)
-            if status_code in status_code_count:
-                status_code_count[status_code] += 1
-            line_count += 1
+        line_count += 1
+
+        parts = line.split()
+        if len(parts) < 7:
+            continue
+
+        status_code = parts[7]
+        file_size = int(parts[8])
+
+        total_file_size += int(file_size)
+
+        if status_code in status_code_count.keys():
+            status_code_count[status_code] += 1
 
         if line_count % 10 == 0:
-            print("File size: {}".format(line_count))
+            print("File size: {}".format(total_file_size))
             for code in sorted(status_code_count.keys()):
                 if status_code_count[code] > 0:
                     print("{}: {}".format(code, status_code_count[code]))
 
-            status_code_count = {code: 0 for code in status_code_count}
 except KeyboardInterrupt:
-    print("File size: {}".format(line_count))
-    for code in sorted(status_code_count.keys(), key=int):
+    print("File size: {}".format(total_file_size))
+    for code in sorted(status_code_count.keys()):
         if status_code_count[code] > 0:
             print("{}: {}".format(code, status_code_count[code]))
-    sys.exit()
+    sys.exit(0)
