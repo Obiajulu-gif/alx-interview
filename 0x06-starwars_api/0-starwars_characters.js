f62de878bf75f479a052901f8bae3e2268d13ed8
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 22:17:11 +0100

    task 0 done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
-"""
-a script that prints all characters of a Star Wars movie
-"""
-const request = require("request");
+/*
+  a script that prints all characters of a Star Wars movie
+*/
+const request = require('request');
 
 const movieID = process.argv[2];
 
 const apiUrl = `https://swapi.dev/api/films/${movieID}/`;
 
 request(apiUrl, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	const movieData = JSON.parse(body);
+  const movieData = JSON.parse(body);
 
-	const characters = movieData.characters;
+  const characters = movieData.characters;
 
-	characters.forEach((characterUrl) => {
-		request(characterUrl, (charError, charResponse, charBody) => {
-			if (charError) {
-				console.error(charError);
-				return;
-			}
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
+      }
 
-			const characterData = JSON.parse(charBody);
+      const characterData = JSON.parse(charBody);
 
-			console.log(characterData.name);
-		});
-	});
+      console.log(characterData.name);
+    });
+  });
 });
